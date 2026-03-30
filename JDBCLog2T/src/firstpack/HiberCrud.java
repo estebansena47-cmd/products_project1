88c33dbee6cedb61a27982df4326ebda270429c6
Freidell Esteban T <estebansena47@gmail.com>
2026-03-30 02:36:20 -0500

    Añadiendo usuario con Hybernate

--- JDBCLog2T/src/firstpack/HiberCrud.java
+++ JDBCLog2T/src/firstpack/HiberCrud.java
@@ -17,15 +17,39 @@ public class HiberCrud {
         
         
         try{
+            String correoEliminar = "paco2@gmail.com" ;
+            String correoConsulta = "zury723@gmail.com" ;
+            Usuario usuarioExistente = session.createQuery ("FROM Usuario WHERE Correo_Electronico = :email", Usuario.class)
+                    .setParameter("email", correoConsulta)
+                    .uniqueResult();
+            Usuario usuarioBorrar = session.createQuery ("FROM Usuario WHERE Correo_Electronico = :emailBorra", Usuario.class)
+                    .setParameter("emailBorra", correoEliminar)
+                    .uniqueResult();
+            if(usuarioExistente == null){
         Usuario user= new Usuario();
-        user.setNombre_Usuario("Andres");
+        user.setNombre_Usuario("Zury");
         user.setNumero_Telefonico("00000000000");
-        user.setCorreo_Electronico("losPrimos@gmail.com");
-        user.setContraseña("andrew3573");
+        user.setCorreo_Electronico(correoConsulta);
+        user.setContraseña("zurys4r41");
         
         session.persist(user);
         t.commit();
-            System.out.println("usuario guardado con exito");
+            System.out.println("usuario guardado con exito");}
+           else{
+            usuarioExistente.setNumero_Telefonico("3139078393");
+            session.merge(usuarioExistente);
+            t.commit();
+                System.out.println("Datos Actualizados");
+            }
+            
+             if(usuarioBorrar != null){
+        session.remove(usuarioBorrar);
+        t.commit();
+            System.out.println("usuario eliminado");}
+           else{
+            
+                System.out.println("No se encontro ningun usuario con ese correo");
+            }
         } catch (Exception e){
         t.rollback();
         e.printStackTrace();
